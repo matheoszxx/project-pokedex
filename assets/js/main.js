9a722987abdf2f7fb01efe5200a51f87e5ea3acd
matheus <matheuszxx1@outlook.com>
2023-09-22 10:36:15 -0300

    chaining of .then, Promise and arrow function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 
-console.log("sucesso!");
+//Querry String Paramets
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+//fetch() fornece uma maneira fácil e lógica de buscar recursos de forma assíncrona na rede.
+fetch(url)
+    //Transformando o response em uma Promise do body convertido em json()
+    .then((response) =>response.json())
+    //Recebe o body convertido e printa na tela
+    .then((bodyConvertidoEmJson) => console.log(bodyConvertidoEmJson))
+    .then((error) => console.error(error))
