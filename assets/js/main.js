169247a59e1f1e774f5b89d3fea2bf11d7c1b706
matheoszxx <matheuszxx1@outlook.com>
2023-09-22 00:14:44 -0300

    chaning .then and convert the body to json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
-
+//Querry String Paraments 
 const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response){
-    console.log(response);
-})
+//fetch() fornece uma maneira fácil e lógica de buscar recursos de forma assíncrona na rede.
+fetch(url)
+//Transformando o response em promise do body convertida em json()
+.then((response) => response.json())
+//Recebe o body convertido e printando na tela
+.then((respondeConvertidoEmJson) => console.log(respondeConvertidoEmJson))
+.catch((error) => console.error(error))
+    
