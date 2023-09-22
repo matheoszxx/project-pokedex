d9c06a950ce4a514f015d5baa15d319ac329b30c
matheus <matheuszxx1@outlook.com>
2023-09-22 11:37:42 -0300

    lists pokemon convert in HTML by JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,46 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+
+            <!-- header do pokemon -->
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <!-- div com os detalhes do pokemon -->
+            <div class="details">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <!-- Imagem do pokemon -->
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                alt="${pokemon.name}">
+            </div>
+
+        </li>
+   `
+}
+
 //fetch() fornece uma maneira fácil e lógica de buscar recursos de forma assíncrona na rede.
 fetch(url)
+
     //Transformando o response em uma Promise do body convertido em json()
-    .then((response) =>response.json())
-    //Recebe o body convertido e printa na tela
-    .then((bodyConvertidoEmJson) => console.log(bodyConvertidoEmJson))
+    .then((response) => response.json())
+
+    //Recebe o body convertido e pega a lista que é o "results"
+    .then((bodyConvertidoEmJson) => bodyConvertidoEmJson.results)
+
+    //Pega a lista de pokemon e printa na tela
+    .then((pokemonList) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToHtml(pokemon));
+            
+        }
+    })
+
     .then((error) => console.error(error))
